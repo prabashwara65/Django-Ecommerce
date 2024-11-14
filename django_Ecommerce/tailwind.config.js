c779f9d760c8b975292f9b913543046a21461666
prabashwara65 <prabashwara65@gmail.com>
2024-11-14 18:13:04 +0530

    designing forntend

--- django_Ecommerce/tailwind.config.js
+++ django_Ecommerce/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./template/**/*.html"],
+  content: ["./template/**/*.html", "./products/templates/**/*.html"],
   theme: {
     extend: {},
   },
