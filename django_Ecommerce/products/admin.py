50c7044295866a9eb6df980b766918edbaed0d0d
prabashwara65 <prabashwara65@gmail.com>
2024-11-21 19:15:34 +0530

    starting to add styles

--- django_Ecommerce/products/admin.py
+++ django_Ecommerce/products/admin.py
@@ -7,3 +7,10 @@ from .models import Category,Product
 class categoryAdmin(admin.ModelAdmin):
     list_display = ["name" , "slug"]
     prepopulated_fields = {'slug' : ('name' , )}
+
+
+
+@admin.register(Product)
+class productAdmin(admin.ModelAdmin):
+    list_display = ["name" , "price" , "available" , "created" , "updated" , "category"]
+    prepopulated_fields = {'slug' : ('name' , )}
