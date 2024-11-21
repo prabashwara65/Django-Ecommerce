a2d5e4e2f9fc28c2b5f549c9dfa506a0fa4f5a08
prabashwara65 <prabashwara65@gmail.com>
2024-11-21 18:56:14 +0530

    registered Category model

--- django_Ecommerce/products/admin.py
+++ django_Ecommerce/products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category,Product
 
 # Register your models here.
+
+admin.register(Category)
+class categoryAdmin(admin.ModelAdmin):
+    list_display = ["name" , "slug"]
+    prepopulated_fields = {"slug" : ('name' , )}
